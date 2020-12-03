6e10d90f9df4314833a20cab25ca341356321dfd
lislis <mail@lislis.de>
2020-12-03 16:24:12 +0100

    feat(foyer) update dockerfile

---
+++ foyer/vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    devServer: {
+        proxy: {
+            'http://localhost:3000': {
+                target: 'http://localhost:3000',
+                ws: true,
+                changeOrigin: true
+            },
+            'http://localhost:4000': {
+                target: 'http://localhost:4000',
+                ws: true,
+                changeOrigin: true
+            }
+        }
+    }
+}
