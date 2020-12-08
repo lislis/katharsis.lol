11ded7c935be6e4fab5dc27db59189658d45d1ba
lislis <mail@lislis.de>
2020-12-08 13:01:42 +0100

    feat(botbrain) factor in flavor and optional and numerus

--- botbrain/lib/utils.js
+++ botbrain/lib/utils.js
@@ -2,56 +2,85 @@ const _ = require('lodash');
 const random = require('random');
 
 function fillPlaceholders(origObj, values, params) {
-    let [word, users] = values;
     let obj = origObj;
-
-    let {flavor, numerus} = params;
 
     const re = /(\#.+?\#)/g;
     const array = [...obj.text.matchAll(re)];
     let output = obj.text;
 
     array.forEach((v, k) => {
-        const re1 = /\#(.+?)\#/;
-        const cat = v[0].match(re1)[1];
+        const re_clean = /\#(.+?)\#/;
+        const catClean = v[0].match(re_clean)[1];
 
-        if (cat == 'User') {
-            output = replaceUser(output, v[0], users);
-        } else {
-            let subs = {};
-            const isOptional = cat.match(/\?.+/);
-
-            if (isOptional) {
-                if (random.boolean()) {
-                    //console.log("isoptional: true")
-                    subs.text = "";
-                } else {
-                    //console.log("isoptional: false");
-                    subs = _.sample(word
-                                    .filter(x => x.word_type == cat.split('?optional')[0])
-                                    .filter(x => x.numerus == obj.numerus
-                                            || !x.numerus ));
-                }
-            } else {
-                subs = _.sample(word
-                                .filter(x => x.word_type == cat)
-                                .filter(x => x.numerus == obj.numerus
-                                        || !x.numerus ));
-            }
-
-            if (subs !== undefined) {
-                output = output.replace(v[0], subs.text);
-            }
-        }
+        let sub = findSub(catClean, obj, values);
+        output = replacePatternWSub(output, v[0], sub);
     });
 
     return [output];
 }
 
+function findSub(category, options, pools) {
+    let [word, users] = pools;
+    let sub = {};
 
-function replaceUser(str, pattern, replacementPool) {
-    let user = _.sample(replacementPool);
-    return str.replace(pattern, user.nickname);
+    if (category == 'User') {
+        sub = sampleUser(users);
+    } else {
+        const isOptional = category.match(/\?.+/);
+
+        if (isOptional) {
+            if (random.boolean()) {
+                sub.text = "";
+            } else {
+                let catNoOptional = category.split('?optional')[0];
+                sub = sampleBy(word, catNoOptional, options);
+            }
+        } else {
+            sub = sampleBy(word, category, options);
+        }
+    }
+    return sub;
+}
+
+function replacePatternWSub(str, pattern, sub) {
+    let txt = "";
+    if (sub != undefined) {
+        if (sub.nickname) {
+            txt = sub.nickname;
+        } else {
+            txt = sub.text;
+        }
+    }
+    return str.replace(pattern, txt);
+}
+
+function sampleBy(pool, category, options) {
+    let flavorFlave = options.direction_type;
+    let cattyCat = category;
+
+    const reOtherFlavor = /_/;
+    let hasExplicitFlavor = category.match(reOtherFlavor);
+
+    if (hasExplicitFlavor) {
+        cattyCat= category.split("_")[0];
+        flavorFlave = category.split("_")[1];
+    }
+
+    let w =  _.sample(pool
+                      .filter(x => x.flavor == flavorFlave)
+                      .filter(x => x.word_type == cattyCat)
+                      .filter(x => {
+                          if (cattyCat == "Verb") {
+                              return x.numerus == options.numerus;
+                          } else {
+                              return x.numerus == options.numerus || !x.numerus;
+                          }
+                      }));
+    return w;
+}
+
+function sampleUser(pool) {
+    return _.sample(pool);
 }
 
 exports.fillPlaceholders = fillPlaceholders;
