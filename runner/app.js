94bb8f9547508ed32deab7e73c533f9b9ff67e40
lislis <mail@lislis.de>
2020-12-29 15:27:57 +0100

    feat(runner) add admin panel

--- runner/app.js
+++ runner/app.js
@@ -31,7 +31,7 @@ app.use(function(req, res, next) {
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended':'false'}));
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.use('/api/', routes);
 
--- runner/routes/index.js
+++ runner/routes/index.js
@@ -8,10 +8,12 @@ const { run } = require('../scheduler.js')
 router.post('/runschedule', function(req, res, next) {
     if (req.body.hasOwnProperty('csvUrl') && req.body.csvUrl !== '') {
         run(req.body.csvUrl).then(r => {
+            console.log("Schedule stated");
             console.log(r);
-            res.json({ message: "worked!" });
+            //res.json({ message: "Schedule started!" });
+            //res.redirect('/');
         });
-
+        res.json({ message: "Schedule started" });
     } else {
         res.json({ message: "No url to csv given" });
     }
