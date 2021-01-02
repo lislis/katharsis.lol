092680640b6d55a368f4f27caddac05560cb53bc
lislis <mail@lislis.de>
2021-01-02 19:28:09 +0100

    chorse(ui) cleanup messy deps

--- foyer/src/main.js
+++ foyer/src/main.js
@@ -1,15 +1,8 @@
 //import Vue from 'vue'
-//import Vuex from 'vuex'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-//import BootstrapVue from 'bootstrap-vue'
-//import 'bootstrap/dist/css/bootstrap.css'
-//import 'bootstrap-vue/dist/bootstrap-vue.css'
-import VueChatScroll from 'vue-chat-scroll'
 
 createApp(App)
     .use(router)
-    .use(VueChatScroll)
-  //  .use(BootstrapVue)
     .mount('#app')
