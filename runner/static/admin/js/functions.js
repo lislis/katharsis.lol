9bfe7e9714069cea647b8e8ba1fa440c676c5219
lislis <mail@lislis.de>
2021-01-05 20:17:22 +0100

    feat(runner) add delete word and direction to admin panel

--- runner/static/admin/js/functions.js
+++ runner/static/admin/js/functions.js
@@ -1,11 +1,11 @@
 async function deleteSingle(input, url, message) {
   let inputElem = document.querySelector(input);
 
-  let response = await fetch(CONFIG.SERVER + url + inputElem.value, {
+  let response = await fetch(url + inputElem.value, {
     method: 'DELETE',
   })
   let obj = await response.json();
-  alert(`${message} ${obj.room_name}`);
+  alert(`${message} \n${JSON.stringify(obj)}`);
   inputElem.value = "";
 }
 
@@ -19,12 +19,12 @@ function buildDeleteFunction(formid, inputid, url, message) {
 async function buildDeleteAllFunction(formId, apiEndpoint, message) {
   document.querySelector(formId).onsubmit = async (e) => {
     e.preventDefault();
-    fetch(`${CONFIG.SERVER}${apiEndpoint}`)
+    fetch(`${apiEndpoint}`)
       .then(r => r.json())
       .then(values =>  {
         Promise.all(
           values.map(x => {
-            fetch(`${CONFIG.SERVER}${apiEndpoint}/${x._id}`, {
+            fetch(`${apiEndpoint}${x._id}`, {
               method: 'DELETE'
             }).then(resp => resp.json())
           })
