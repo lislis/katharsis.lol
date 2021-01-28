67372f2d97859f651f5684d9cafdd1abf9adcd77
lislis <mail@lislis.de>
2021-01-28 15:47:13 +0100

    feat(server) add colorCode to user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    nickname: String,
-    hasPermission: { type: Boolean, default: false },
-    isMod: { type: Boolean, default: false },
-    created_date: { type: Date, default: Date.now },
+  nickname: String,
+  hasPermission: { type: Boolean, default: false },
+  isMod: { type: Boolean, default: false },
+  colorCode: { type: String, default: '#ffffff' },
+  created_date: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('User', UserSchema);
