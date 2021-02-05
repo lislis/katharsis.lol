9e0b95134254dbe8bcaee78006dde1c07ee40650
lislis <mail@lislis.de>
2021-02-05 22:33:08 +0100

    feat(ui) add fields and error handling for ticket code

--- ui/src/components/AppHeader.vue
+++ ui/src/components/AppHeader.vue
@@ -5,30 +5,19 @@
       <UserPanel />
     </div>
 
-    <div class="notifications" v-if="notifications.length">
-      <div v-for="(note, id) in notifications" :key="id"  class="note">
-        <p>{{note}}</p>
-        <button @click.prevent="removeNotification(id)"
-                type="button"
-                class="btn"
-                :aria-label="$t('ui.button.close')">{{ $t('ui.button.accept') }}</button></div>
-    </div>
+    <Notifications :notifications="notifications" />
   </div>
 </template>
 <script>
+ import Notifications from '@/components/Notifications'
  import UserPanel from '@/components/UserPanel'
- import { removeByIndex } from '@/lib/utils'
 
  export default {
    name: "AppHeader",
    props: ['notifications'],
    components: {
-     UserPanel
-   },
-   methods: {
-     removeNotification(ev, id) {
-       removeByIndex(this.$root.$data.notifications, id)
-     }
+     UserPanel,
+     Notifications
    }
  }
 </script>
@@ -39,28 +28,5 @@
    justify-content: space-between;
    align-items: center;
    padding: 0 1rem;
- }
- .notifications {
-   text-align: center;
-   position: absolute;
-   top: 0;
-   left: 0;
-   right: 0;
-   width: 100%;
-   height: 100%;
-   background-color: rgba(100, 100, 100, 0.8);
-   z-index: 10;
-
- }
- .note {
-   padding: 1em;
-   position: absolute;
-   background-color: white;
-   top: 50%;
-   left: 50%;
-   transform: translate(-50%, -50%);
-   background: white;
-   border-radius: 4px;
-
  }
 </style>
---
+++ ui/src/components/Notifications.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="notifications" v-if="notifications.length">
+    <div v-for="(note, id) in notifications" :key="id"  class="note">
+      <p>{{note}}</p>
+      <button @click.prevent="removeNotification(id)"
+              type="button"
+              class="btn"
+              :aria-label="$t('ui.button.close')">{{ $t('ui.button.accept') }}</button></div>
+  </div>
+</template>
+<script>
+ import { removeByIndex } from '@/lib/utils'
+  export default {
+    name: 'Notifications',
+    props: ['notifications'],
+    methods: {
+      removeNotification(ev, id) {
+        removeByIndex(this.$root.$data.notifications, id)
+      }
+    }
+  }
+</script>
+<style scoped>
+ .notifications {
+   text-align: center;
+   position: absolute;
+   top: 0;
+   left: 0;
+   right: 0;
+   width: 100%;
+   height: 100%;
+   background-color: rgba(100, 100, 100, 0.8);
+   z-index: 10;
+
+ }
+ .note {
+   padding: 1em;
+   position: absolute;
+   background-color: white;
+   top: 50%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+   background: white;
+   border-radius: 4px;
+ }
+</style>
