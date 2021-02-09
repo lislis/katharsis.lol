fb0c5a0793b1a557611abf5d6509bbd0fa5144b6
lislis <mail@lislis.de>
2021-02-09 19:23:48 +0100

    feat(runner) make last action to automatically rm people from stage

--- runner/jobs/endOfSchedule.js
+++ runner/jobs/endOfSchedule.js
@@ -6,6 +6,7 @@ const logger = require('../lib/logger').logger;
 
 const SERVER = process.env['SERVER_URL'];
 
+/*
 axios.post(`${SERVER}/api/script/theend`).then(() => {
   logger.info("[job] The end!");
   if (parentPort) parentPort.postMessage('done');
@@ -13,4 +14,14 @@ axios.post(`${SERVER}/api/script/theend`).then(() => {
 }).catch(e => {
   console.log(e);
   process.exit(1);
+});
+*/
+
+axios.post(`${SERVER}/api/script/bulkOffStage`).then(() => {
+    logger.info("[job] everyone off!");
+    if (parentPort) parentPort.postMessage('done');
+    else process.exit(0);
+}).catch(e => {
+    console.log(e);
+    process.exit(1);
 });
