412a49f5f25c5b7735430025f319830c0ae1c196
lislis <mail@lislis.de>
2021-02-11 19:03:52 +0100

    feat(server) lighthouse fixes

--- server/lib/platform_embeds.js
+++ server/lib/platform_embeds.js
@@ -1,9 +1,9 @@
 const platforms = {
     youtube: {
-        code: `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/##ID##?controls=0" frameborder="0" allowfullscreen></iframe>`
+        code: `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/##ID##?controls=0" frameborder="0" title="YouTube embed" allowfullscreen></iframe>`
     },
     soundcloud: {
-        code: `<iframe width="100%" height="315" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/##ID##&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`
+        code: `<iframe width="100%" height="315" scrolling="no" title="SoundCloud embed" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/##ID##&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>`
     }
 };
 
