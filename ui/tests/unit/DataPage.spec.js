49418072e596d42090e75b57d1dc3712d6bff095
lislis <mail@lislis.de>
2021-02-17 21:37:12 +0100

    fix(ui) fix tests

--- ui/tests/unit/DataPage.spec.js
+++ ui/tests/unit/DataPage.spec.js
@@ -5,9 +5,9 @@ import DataPage from '@/views/DataPage';
 describe('DataPage.vue', () => {
     const wrapper = shallowMount(DataPage);
     it('renders the title', () => {
-        expect(wrapper.find('.index__title').element).toBeVisible();
+        expect(wrapper.find('h1').element).toBeVisible();
     });
     it('renders the copy', () => {
-        expect(wrapper.find('.index__inner').element).toBeVisible();
+        expect(wrapper.find('.index__overflow').element).toBeVisible();
     });
 });
